d9640cfb470ce243db5845f81ed1ee436aa54136
sibom007 <vaispark400@gmail.com>
2023-05-31 00:56:11 -0700

    new add

--- src/Pages/Deshbord/All user/Alluser.jsx
+++ src/Pages/Deshbord/All user/Alluser.jsx
@@ -3,13 +3,14 @@ import { useQuery } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async';
 import { FaTrash, FaUserShield } from 'react-icons/fa';
 import Swal from 'sweetalert2';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 
 const Alluser = () => {
 
-
+    const [axiosSecure] = useAxiosSecure();
     const { data: users = [], refetch } = useQuery(['users'], async () => {
-        const res = await fetch('http://localhost:5000/users')
-        return res.json();
+        // const res = await axiosSecure.get('/users')
+        // return res.data;
     })
 
     const handlerdadmin = user =>{
